c379682569c34e881efb4da7e6d9ce77e1f4652b
Axelhrdz <axelhernandezperez23@gmail.com>
2024-09-11 22:17:49 -0600

    working on adding meals function

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ const form = document.querySelector('form');
 const inputBudget = document.getElementById('inputBudget');
 const budget = document.getElementById('budget');
 const budgetHeadline = document.querySelector('.budgetHeadline');
+//Meals
+const meals = document.getElementById('meals');
+const btnAddMeal = document.getElementById('btnAddMeal');
 
+console.log(meals.value);
+
+console.log(btnAddMeal);
+console.log(meals);
+console.log(meals.firstElementChild);
+// btnAddMeal.onclick = function(){
+//     console.log(meals.childNodes)
+// }
+
+
+//Submiting budget
+let budgetSubmitted;
 form.addEventListener('submit', function(e){
     e.preventDefault();
     console.log(inputBudget.value);
-    let budgetSubmitted = inputBudget.value;
+    budgetSubmitted = inputBudget.value;
     budget.innerText = budgetSubmitted;
     inputBudget.value = "";
 
@@ -14,15 +29,58 @@ form.addEventListener('submit', function(e){
 })
 
 
-// //Entry meals
-// console.log(test);
-// for(meal of test){
-//     console.log(meal.id);
-// }
+//Submiting type meals entries
+const breakContainer = document.querySelector('.breakContainer');
+const lunchContainer = document.querySelector('.lunchContainer');
+const dinnerContainer = document.querySelector('.dinnerContainer');
+const snacksContainer = document.querySelector('.snacksContainer');
+// console.log(breakContainer)
+// const test = document.createElement('p')
+// test.innerText = 'this is a test'
+// breakContainer.append(test)
+
+function createMeal(container){
+    const inputMeal = document.createElement('input');
+    const inputCalorie = document.createElement('input');
+
+    container.append(inputMeal);
+    container.append(inputCalorie);
+}
+
+let currentMealType = meals.value;
+let mealContainer;
+meals.addEventListener('change', function(){
+    // console.log(this.value);
+    currentMealType = this.value;
+    mealContainer = `${this.value}Container`;
+    // console.log(mealContainer);
+    // console.log(currentMealType);
 
 
-// entryMealBtn.addEventListener('click', function(){
-//     console.log('meal added');
+    switch(currentMealType){
+        case 'breakfast':
+            mealContainer = breakContainer;
+            break;
+        case 'lunch':
+            mealContainer = lunchContainer;
+            break;
+        case 'dinner':
+            mealContainer = dinnerContainer;
+            break;
+        case 'snacks':
+            mealContainer = snacksContainer;
+            break;
+        default:
+            mealContainer = meals.value;
+    }
 
-//     console.log(mealsDropdown);
-// })
+})
+
+// btnAddMeal.addEventListener('click', createMeal(mealContainer));
+
+btnAddMeal.addEventListener('click', function(){
+    if(mealContainer){
+        createMeal(mealContainer);
+
+    }
+});
